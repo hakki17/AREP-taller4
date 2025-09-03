9ec70d3cc7dc9cb5935e9bca6046660bd8bf9576
unknown <maria.sanchez-m@labinfo.is.escuelaing.edu.co>
2025-09-03 11:10:07 -0500

    add: dockeraws

---
+++ src/main/java/co/escuelaing/arep/dockeraws/DockerAWS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.escuelaing.arep.dockeraws;
+
+/**
+ *
+ * @author maria.sanchez-m
+ */
+public class DockerAWS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
